2b3c2a367d758fdeb65f2f8288bbb70dca40ba09
Thijs van den hout <36235544+tvdhout@users.noreply.github.com>
2019-04-17 19:21:35 +0200

    testing networkx graph, minor restructuring

---
+++ main.py
@@ -0,0 +1,30 @@
+from superpermutations import *
+from networkx_test import *
+
+n = 7
+print("n =",n)
+# Thijs implementation
+start = time()
+thijs_graph = get_graph(n)
+print('Thijs graph constructed in {} sec'.format(time()-start))
+
+start = time()
+thijs_superperm = path_to_string(greedy_tsp_search(thijs_graph))
+print("Thijs greedy search completed in {} sec\n".format(time()-start))
+
+assert "Not a valid superpermutation", check_string(n, thijs_superperm)
+
+
+# Networkx implementaion
+start = time()
+permutations = get_permutations(n)
+edges = get_edges(permutations)
+
+graph = nx.DiGraph()
+graph.add_weighted_edges_from(edges)
+print("networkx graph constructed in {} sec".format(time()-start))
+
+start = time()
+superperm = path_to_string(greedy_networkx(graph))
+print("networkx greedy search completed in {} sec".format(time() - start))
+assert "Not a valid superpermutation", check_string(n, superperm)
--- networkx_test.py
+++ networkx_test.py
@@ -1,7 +1,63 @@
 import networkx as nx
 from util import *
+from time import time
 
-graph = nx.DiGraph()
-graph.add_nodes_from(get_permutations(3))
 
-print(graph.nodes)
+def get_edges(perms):
+    """
+    get weighted edges in the form (v1, v2, weight) between all permutations
+    :param perms: permutations to compute edges for
+    :return: list of edges
+    """
+    edges = []
+    for p in perms:
+        for p_other in perms:
+            if p != p_other:
+                edges.append((p, p_other, distance_between(p, p_other)))
+    return edges
+
+
+def greedy_networkx(graph: nx.DiGraph):
+    """
+    Greedy search for a Hamiltonian path through the graph (traveling salesman style)
+    :param graph:
+    :return:
+    """
+    current = list(graph.nodes)[0]  # start with first permutation
+    next_node = None
+    path = []
+
+    while graph.number_of_nodes() > 0:
+        path.append(current)  # add current permutation to path
+        #  find next node with shortest path
+        try:
+            next_node = min(graph.edges(current, data=True), key=lambda e: e[2].get('weight'))[1]
+        except ValueError:  # The last node has no more edges, so min() will be empty
+            pass  # still add the node to the path though, so continue the code
+        graph.remove_node(current)  # remove current from the graph
+        current = next_node
+
+    path.append(current)  # add final node to graph
+    return path
+
+
+if __name__ == '__main__':
+    n = 6
+
+    start = time()
+    permutations = get_permutations(n)
+    edges = get_edges(permutations)
+
+    graph = nx.DiGraph()
+    graph.add_weighted_edges_from(edges)
+    print("Graph constructed in {} sec".format(time()-start))
+
+    # print(graph.edges(data=True))
+    #
+    # print(min(graph.edges(data=True), key=lambda e: e[2].get('weight'))[1])
+
+    # print(graph.edges('231', data=True))
+    start = time()
+    superperm = path_to_string(greedy_networkx(graph))
+    print("Greedy search completed in {} sec".format(time() - start))
+    assert "Not a valid superpermutation", check_string(n, superperm)
